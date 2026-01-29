9421332ab6a6e624f2d505e786a82e0ad4855c35
TopDataNBA1 <TopDataNBA1@users.noreply.github.com>
2026-01-29 03:11:45 +0000

    deploy: 42c6d8a4b8f4c48af35f6a03c06a8ddb74610fb1

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
-// OneSignal Service Worker
-// Este archivo debe estar en la raíz de tu sitio web
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
